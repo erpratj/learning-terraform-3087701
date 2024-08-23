6ed0054c644fe943a2550950c4d0a74e8935d7d1
erpratj <er.pratj@gmail.com>
2024-08-23 19:49:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "BLOG_security-group" {
   version = "5.1.2"
   name = "Blog_sec"
 
-  ingress_rulew=["http-80-tcp","https-443-tcp"]
+  ingress_rules=["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks=["0.0.0.0/0"]
 
   egress_rules=["all-all"]
